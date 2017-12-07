ca4d259f742663e6381f1a69a5eaab671894c180
smart86029 <smart86029@gmail.com>
2017-12-07 22:50:18 +0800

    關閉程式詢問

--- RaffleDraw.Wpf/MainWindow.xaml.cs
+++ RaffleDraw.Wpf/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using MahApps.Metro.Controls;
+using System.ComponentModel;
+using MahApps.Metro.Controls.Dialogs;
 
 namespace RaffleDraw.Wpf
 {
@@ -21,9 +23,40 @@ namespace RaffleDraw.Wpf
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        private bool shouldClose;
+
         public MainWindow()
         {
             InitializeComponent();
+            Closing += MainWindowClosingAsync;
+        }
+
+        private async void MainWindowClosingAsync(object sender, CancelEventArgs e)
+        {
+            if (e.Cancel)
+                return;
+
+            e.Cancel = !shouldClose;
+            if (shouldClose)
+                return;
+
+            var settings = new MetroDialogSettings
+            {
+                AffirmativeButtonText = "是",
+                NegativeButtonText = "否",
+                AnimateShow = true
+            };
+            var result = await this.ShowMessageAsync("詢問", "是否要離開?", MessageDialogStyle.AffirmativeAndNegative, settings);
+
+            shouldClose = result == MessageDialogResult.Affirmative;
+            if (shouldClose)
+                Close();
+
+            //if (!isSaved)
+            //    if (MessageBox.Show("變更還沒有儲存，是否要匯出Excel?", "詢問", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            //        SaveWinningExcel(System.IO.Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName) + "\\中獎名單" + string.Format("{0:MM}", DateTime.Now) + DateTime.Now.Day + ".xlsx");
+            //if (MessageBox.Show("是否要離開?", "詢問", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.No)
+            //    e.Cancel = true;
         }
     }
 }
